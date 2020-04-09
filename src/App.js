5f1c5191662db873e04a3c4a910c75be1ed9f716
maclarea021 <maclarea021@gmail.com>
2020-04-09 18:36:40 -0500

    Fifth Commit

--- src/App.js
+++ src/App.js
@@ -23,98 +23,139 @@ function App() {
   <div className="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
 </div>
 
-<div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-  <ol className="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+<div>
+  <h1 className="textnav">Maria Clara Restrepo Areiza</h1>
+  <h3 className="textheader">Desarrolladora Junior Front-End</h3>
+  <h4 className="textheader">"Si dejas salir tus miedos, tendras mas espacio para vivir tus sueños"</h4>
+  <h5 className="textheader">-Marilyn Monroe-</h5>
+</div>
+
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
   </ol>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img src="./images/carrusel1.gif" className="d-block w-100" alt="..."/>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./images/carrusel1.gif" class="d-block w-100" alt="..."/>
+      <div class="carousel-caption d-none d-md-block">
+        <h5 className="textcarrusel">|Soy una desarrolladora pre-junior Front-end con enfasis en React|</h5>
+      </div>
     </div>
     <div class="carousel-item">
-      <img src="./images/carrusel2.jpg" className="d-block w-100" alt="..."/>
+      <img src="./images/carrusel2.jpg" class="d-block w-100" alt="..."/>
+      <div class="carousel-caption d-none d-md-block">
+        <h5 className="textcarrusel">¡BIENVENIDOS! espero disfruten este pequeño recorido por mi vida</h5>
+      </div>
     </div>
-    <div className="carousel-item">
-      <img src="./images/carrusel3.jpeg" className="d-block w-100" alt="..."/>
+    <div class="carousel-item">
+      <img src="./images/carrusel3.jpeg" class="d-block w-100" alt="..."/>
+      <div class="carousel-caption d-none d-md-block">
+        <h5 className="textcarrusel">mi vida enfocada a la programacion y el diseño, esa pequeña parte de mi que denominan como el Front-End...</h5>
+      </div>
     </div>
   </div>
-  <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="sr-only">Previous</span>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
   </a>
-  <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="sr-only">Next</span>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
   </a>
 </div>
 
-        <div  className="confgtext">
-          <div className="alert portafolio aligntext container1" role="alert">
-      <p id="Acerca">¡BIENVENIDOS! espero disfruten este pequeño recorido por mi vida</p>
-      <p>especificamente mi vida enfocada a la programacion y el diseño</p>
-      <p>esa pequeña parte de mi que denominan como el Front-End...</p>
-          </div>
-          <div className="alert aligntext container1" role="alert">
         
-      <p>WELCOME! I hope you enjoy this little tour of my life</p>
-      <p>specifically my life focused on programming and design</p>
-      <p>that little part of me that they call the Front-End ...</p> 
-    </div> 
+
+      <div className="card CARD mb-3">
+  <div className="row no-gutters">
+    <div className="col-md-4">
+      <img src="./images/html5.png" width="160"  alt="..."/>
     </div>
+    <div className="col-md-8">
+      <div className="card-body">
+        <h5 className="card-title">HTML</h5>
+        <p id="Acerca"className="textnav card-text">es un lenguaje de marcado que se utiliza para el desarrollo de páginas de Internet. Se trata de la sigla que corresponde a HyperText Markup Language, es decir, Lenguaje de Marcas de Hipertexto, que podría ser traducido como Lenguaje de Formato de Documentos para Hipertexto.</p>
+        <p className="textcard2">Manejo un 80/100</p>
+      </div>
+    </div></div>
+    </div>
+    <div className="card CARD mb-3">
+  <div className="row no-gutters">
+    <div className="col-md-4">
+      <img src="./images/css.png" width="160"  alt="..."/>
+    </div>
+    <div className="col-md-8">
+      <div className="card-body">
+        <h5 className="card-title">CSS</h5>
+        <p className="textnav card-text">CSS son las siglas de Cascading Style Sheets - Hojas de Estilo en Cascada - que es un lenguaje que describe la presentación de los documentos estructurados en hojas de estilo para diferentes métodos de interpretación, es decir, describe como se va a mostrar un documento en pantalla, por impresora, por voz o en dispositivos táctiles basados en Braille.</p>
+        <p className="textcard2">Manejo un 80/100</p>
+      </div>
+    </div></div>
+    </div>
+    <div className="card CARD mb-3">
+  <div className="row no-gutters">
+    <div className="col-md-4">
+      <img src="./images/javascript.png" width="160"  alt="..."/>
+    </div>
+    <div className="col-md-8">
+      <div className="card-body">
+        <h5 className="card-title">JavaScript</h5>
+        <p className="textnav card-text">Javascript es una especie de lenguaje de programación ligera, interpretado por la mayoría de los navegadores y que les proporciona a las páginas web, efectos y funciones complementarias a las consideradas como estándar HTML Este tipo de lenguaje de programación, con frecuencia son empleados en los sitios web, para realizar acciones en el lado del cliente, estando centrado en el código fuente de la página web.</p>
+        <p className="textcard2">Manejo un 50/100</p>
+      </div>
+     </div>
+    </div></div>
+    <div className="card CARD mb-3">
+  <div className="row no-gutters">
+    <div className="col-md-4">
+      <img src="./images/react.png" width="160"  alt="..."/>
+    </div>
+    <div className="col-md-8">
+      <div className="card-body">
+        <h5 className="card-title">ReactJS</h5>
+        <p className="textnav card-text">es una biblioteca escrita en JavaScript, desarrollada en Facebook para facilitar la creación de componentes interactivos, reutilizables, para interfaces de usuario. Se utiliza en Facebook para la producción de componentes, e Instagram está escrito enteramente en React. Uno de sus puntos más destacados, es que no sólo se utiliza en el lado del cliente, sino que también se puede representar en el servidor, y trabajar juntos.</p>
+        <p className="textcard2">Manejo un 40/100</p>
+      </div>
+    </div></div>
+    
+ 
     <div className="container1">
-      <img src="./images/Ciudad.jpg" width="1350"/>
+      <img src="./images/Ciudad.jpg" width="1350" height="400"/>
     </div>
-    <div >
-          <section className="portafolio"/>
-           <article className="portafolio_box">
-               <div className=" portafolio_card">
-                 <p>
-                   <img src="./images/nube.png" width="40" alt=""/>
-                   <img  src="./images/sistema-solar.png" width="40"  alt=""/>
-                   <img src="./images/alien.png" width="40"  alt=""/>
-                   <img src="./images/secuestro.png" width="40"  alt=""/></p>
-                   <h3 className="confgtext">Un tributo al universo</h3>
-                   <p className="textnav">Una pagina para que te informes acerca del universo, la ciencia y la astrologia.</p>
-                   <a href='https://mariac021.github.io/tributepage/theuniverse.html'><button >Ver proyecto</button></a>
+    </div>
 
+    <div className="portafolio_card card-deck">
+  <div className="portafolio_card card">
+    <img src="./images/imagen1.png" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 className="textheader card-title">The Universe</h5>
+      <p className="textnav card-text">Una pagina para que te informes acerca del universo, la ciencia y la astrologia.</p>
+      <a href='https://mariac021.github.io/tributepage/theuniverse.html'><button >Ver proyecto</button></a>
 
-               </div>
-
-           </article>
-           <article className="portafolio_box">
-              <div className=" portafolio_card">
-              <p>
-                   <img src="./images/seta.png" width="40" alt=""/>
-                   <img  src="./images/magdalena.png" width="40"  alt=""/>
-                   <img src="./images/casa.png" width="40"  alt=""/>
-                   <img src="./images/estrella.png" width="40"  alt=""/></p>
-                  <h3 className="confgtext">Formulario para la app rappi</h3>
-                  <p className="textnav">Este es un formulario de satisfacción para los usuarios de Rappi.</p>
+    </div>
+  </div>
+  <div className="portafolio_card card">
+    <img src="./images/imagen2.png" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 className="textheader card-title">Formulario para Rappi</h5>
+      <p className="textnav card-text">Este es un formulario de satisfacción para los usuarios de Rappi.</p>
                   <a href='https://mariac021.github.io/Formulario/'><button >Ver proyecto</button>
                   </a>
+    </div>
+  </div>
+  <div className="portafolio_card card">
+    <img src="./images/imagen3.png" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 className="textheader card-title">Make up Colombia</h5>
+      <p className="textnav card-text">Mi pagina de maquillaje en la que muestro mis trabajos,vendo productos y doy consejos.</p>
+      <a href='https://mariac021.github.io/Producto/'> <button >Ver proyecto</button>
+      </a>
+    </div>
+  </div>
+</div>
 
-              </div>
-
-          </article>
-           
-            <article className="portafolio_box">
-               <div className=" portafolio_card">
-               <p>
-                   <img src="./images/cabello.png" width="40" alt=""/>
-                   <img  src="./images/boca.png" width="40"  alt=""/>
-                   <img src="./images/pastel.png" width="40"  alt=""/>
-                   <img src="./images/sombra.png" width="40"  alt=""/></p>
-                   <h3 className="confgtext">Mi pagina de Maquillaje</h3>
-                   <p className="textnav">Mi pagina de maquillaje en la que muestro mis trabajos,vendo productos y doy consejos.</p>
-                   <a href='https://mariac021.github.io/Producto/'> <button >Ver proyecto</button>
-                   </a>
- 
-               </div>
-
-          </article>
-       </div>
 
        <footer className="footer">
   <div className="container">
